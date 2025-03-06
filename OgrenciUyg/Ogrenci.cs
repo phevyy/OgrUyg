b15f3d5464d27a7673097aa18a871776bdea628b
Fikret Emre Sınmaz <fikreddemre@gmail.com>
2025-03-07 02:33:20 +0300

    OgrUyg
    Öğrencinin bilgileri kod bloklarında yazılan şekilde gösteren basit bir uygulama

---
+++ OgrenciUyg/Ogrenci.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciUyg
+{
+    public class Ogrenci
+    {
+        private int ogrenciNo;
+        private string isim;
+        private string soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okulismi;
+
+        public Ogrenci(int _ogrenciNo, string _isim, string _soyisim, int _vize1, int _vize2, int _final, string _okulismi)
+        {
+            ogrenciNo = _ogrenciNo;
+            isim = _isim;
+            soyisim = _soyisim;
+            vize1 = _vize1;                        // Yapıcı metot yazılışı
+            vize2 = _vize2; 
+            final = _final;
+            okulismi = _okulismi;
+        }
+
+        public void OgrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrencinin Numarası : " + ogrenciNo);
+            Console.WriteLine("Öğrencinin ismi : " + isim);
+            Console.WriteLine("Öğrencinin soyismi : " + soyisim);
+            Console.WriteLine("Öğrencinin Vize1 Notu : " + vize1);
+            Console.WriteLine("Öğrencinin Vize2 Notu : " + vize2);
+            Console.WriteLine("Öğrencinin Final Notu : " + final);
+            Console.WriteLine("Öğrencinin Okul İsmi : " + okulismi);
+        }
+        public double OgrenciOrtalamasiBul()
+        {
+            double ortalama = vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+            return ortalama;
+        }
+        public void okulGetir()
+        {
+            Console.WriteLine("Okul ismi : " + okulismi);
+        }
+
+    }
+
+
+}
---
+++ OgrenciUyg/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciUyg
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogrenci1 = new Ogrenci(3, "Fikret", "Sınmaz", 75, 80, 90, "Necmettin Erbakan"); //yapıcı metodu burada kullandık
+
+            Console.WriteLine("Yapmak istediğiniz işlemi seçiniz");
+            
+
+            
+
+            while (kontrol)
+            {
+                IslemleriGoster();
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.OgrenciBilgileriGoster();
+                        break;
+
+                    case "2":
+                        double ogrenciOrtalama = ogrenci1.OgrenciOrtalamasiBul();
+                        Console.WriteLine("Öğrencinin ortalaması : " + ogrenciOrtalama);
+                        break;
+
+                    case "3":
+                        ogrenci1.okulGetir();
+                        break;
+
+                    case "4":
+                        kontrol = false;
+                        break;
+
+
+
+                }
+            }
+
+        }
+            static void IslemleriGoster()
+        {
+            Console.WriteLine("1 - Öğrenci bilgileri göster");
+            Console.WriteLine("2 - Öğrencinin ortalamasını göster");
+            Console.WriteLine("3 - Öğrencinin okulunu göster");
+            Console.WriteLine("4 - Çıkış");
+        }
+    }
+}
